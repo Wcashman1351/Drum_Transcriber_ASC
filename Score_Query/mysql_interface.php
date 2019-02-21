9dced782a202093bee8fb9150cd32c66cdf61891
Wcashman1351 <wcashman1351@gmail.com>
2019-02-21 14:02:32 +1100

    Table highlighting working and jquery integrated nicely

--- Score_Query/mysql_interface.php
+++ Score_Query/mysql_interface.php
@@ -7,38 +7,45 @@
         die( '<p>Unable to connect to the database server.</p>' );
     }
 
-    if (isset($_POST['get_scores'])) {
+    switch ($_POST['RequestType']) {
+        case "Share": ShareFunction($_Post['Username']); break;
+        case "Login": LoginFunction($_POST['Username'], $_POST['scoreData'], $_POST['scoreDate']); break;
+             default: die();
+    }
+
+    function LoginFunction($Username, $Password) {
+  
         // Checks Username and Password is valid
-        $query = 'SELECT * FROM ACCOUNTS WHERE Username="'.$_POST['Username'].'"'. ' AND Password="'.$_POST['Password'].'";';
+        $query = 'SELECT * FROM ACCOUNTS WHERE Username="'.$Username.'"'.' AND Password="'.$Password.'";';
         if (!$dbcnx->query($query)) {
             die( '<p>Incorrect Username or Password. Error: '.$dbcnx->error.'.</p>' );
         }
         
         // Retreives musical scores
-        $query = "SELECT * FROM ".$_POST['Username'];
+        $query = "SELECT * FROM ".$Username;
         if ($result = $dbcnx->query($query)) {
             // If query is successful, then it puts all the entries into an array and returns it
             while ($row = $result->fetch_assoc()) {
                 $arr[] = $row;
             }
-            echo json_encode($arr);
+            header('Location: ViewScores.html');
+            return json_encode($arr);
         } else {
             die ("<p>Query unsuccessful</p>");
         }
-    } else if (isset($_POST['share_score'])) {
+
+    } 
+    function ShareFunction($Username, $ScoreData, $ScoreDate) {
         // Checks username is valid
-        $query = "SELECT * FROM ACCOUNTS WHERE User=".$_POST['Username'].";";
+        $query = 'SELECT * FROM ACCOUNTS WHERE Username="'.$Username.'";';
         if (!$dbcnx->query($query)) {
-            die( '<p>Username does not exist. Try again.</p>' );
+            return ('Username does not exist. Try again.');
         }
         // Insert musical score
-        $query = "INSERT INTO ".$_POST['Username']."(Score, Data) VALUES ("
-                    .$_POST['data_entry']['score'].", ".$_POST['data_entry']['Date'].");";
-        if ($dbcnx->query($query)) {
-            echo json_encode("Succesdfully Added");
-        } else {
-            echo json_encode("Error adding score");
-        }
+        $query = 'INSERT INTO "'.$Username.'" VALUES (, "'
+                    .$ScoreData.'", "'.$ScoreDate.'");';
+        
+        echo ($dbcnx->query($query) ? "Succesdfully Added" : "Error adding score");
     }
 
 ?>
