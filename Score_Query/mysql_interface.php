9425db18c971fa2a1ac02ecbb30a0a1c7bcb99e3
Wcashman1351 <wcashman1351@gmail.com>
2019-02-25 21:31:24 +1100

    Final Commit

--- Score_Query/mysql_interface.php
+++ Score_Query/mysql_interface.php
@@ -4,28 +4,25 @@
     // Gets the musical scores from the MySQL database
 
     switch ($_POST['RequestType']) {
-        case 'Share': echo ShareFunction($_POST['Username'], $_POST['scoreData'], $_POST['scoreDate']); break;
-        case 'Login': echo LoginFunction($_POST['Username'], $_POST['Password']); break;
-        case 'Submit': SubmitFunction($_POST['addscore']); break;
-             default: die("Not a valid request");
+        case 'Share' : echo ShareFunction($_POST['Username'], $_POST['scoreData'], $_POST['scoreDate']); break;
+        case 'Login' : echo LoginFunction($_POST['Username'], $_POST['Password']);                       break;
+        case 'Submit': SubmitFunction($_POST['addscore']);                                               break;
+        default      : die("Not a valid request");
     }
 
     function LoginFunction($Username, $Password) {
   
-        $dbcnx = new mysqli('127.0.0.1', 'root', '123456', 'drum_scores');
-        if (!$dbcnx) {
-            die( '<p>Unable to connect to the database server.</p>' );
-        }
+        $sqlObj = ConnectToMySQL(); // Establish connection to MySQL Server
        
         // Checks Username and Password is valid
         $query = 'SELECT * FROM ACCOUNTS WHERE Username="'.$Username.'"'.' AND Password="'.$Password.'";';
-        if (!$dbcnx->query($query)) {
-            die( '<p>Incorrect Username or Password. Error: '.$dbcnx->error.'.</p>' );
+        if (!$sqlObj->query($query)) {
+            die( '<p>Incorrect Username or Password. Error: '.$sqlObj->error.'.</p>' );
         }
-        
+        $arr = [];
         // Retreives musical scores
         $query = "SELECT * FROM ".$Username;
-        if ($result = $dbcnx->query($query)) {
+        if ($result = $sqlObj->query($query)) {
             // If query is successful, then it puts all the entries into an array and returns it
             // Total array is standard numbered. But each row array is indexed by the column names
             while ($row = $result->fetch_assoc()) {
@@ -39,34 +36,36 @@
     } 
     function ShareFunction($Username, $ScoreData, $ScoreDate) {
 
-        $dbcnx = new mysqli('127.0.0.1', 'root', '123456', 'drum_scores');
-        if (!$dbcnx) {
-            die( '<p>Unable to connect to the database server.</p>' );
-        }
+        $sqlObj = ConnectToMySQL(); // Establish connection to MySQL Server
 
         // Checks username is valid
         $query = 'SELECT * FROM ACCOUNTS WHERE Username="'.$Username.'";';
-        if (!($dbcnx->query($query))) {
+        if (!($sqlObj->query($query))) {
             die('Username does not exist. Try again.');
         }
         // Insert musical score
         $query = 'INSERT INTO '.$Username.' (ScoreValue, Date) VALUES ("'
                     .$ScoreData.'", "'.$ScoreDate.'");';
         
-        echo $dbcnx->query($query) ? "Succesdfully Added" : "Error adding score";
+        echo $sqlObj->query($query) ? "Succesdfully Added" : "Error adding score";
     }
 
     function SubmitFunction($Score) {
-        $dbcnx = new mysqli('127.0.0.1', 'root', '123456', 'drum_scores');
-        if (!$dbcnx) {
+        
+        $sqlObj = ConnectToMySQL(); // Establish connection to MySQL Server
+
+        $query = 'INSERT INTO ADMIN SET ScoreValue="'.$Score.'", Date=CURDATE()';
+
+        echo $sqlObj->query($query) ? "Your score has been added" 
+                                 : '<p>Error adding submitted score: '.mysql_error();
+    }
+
+    function ConnectToMySQL() {
+        $sqlObj = new mysqli('127.0.0.1', 'root', '123456', 'drum_scores');
+        if (!$sqlObj) {
         die( '<p>Unable to connect to the ' .
             'database server at this time.</p>' );
         }
-        // If a score  has been submitted,
-        // add it to the database
-        $sql = 'INSERT INTO ADMIN SET ScoreValue="'.$Score.'", Date=CURDATE()';
-
-        echo $dbcnx->query($sql) ? "Your score has been added" 
-                                 : '<p>Error adding submitted score: '.mysql_error();
+        return $sqlObj;
     }
 ?>
--- Score_Query/submit.php
+++ Score_Query/submit.php
@@ -4,22 +4,17 @@
     // _GET is an array that is generated upon the HTTP request and it contains the url strings.
     // the _GET array can be indexed using the variable's name
     // Connect to the database server
-    $dbcnx = new mysqli('127.0.0.1', 'root', '123456', 'drum_scores');
-    if (!$dbcnx) {
-      die( '<p>Unable to connect to the ' .
-           'database server at this time.</p>' );
-    }
+    $sqlObj = new mysqli('127.0.0.1', 'root', '123456', 'drum_scores');
+        if (!$sqlObj) {
+          die( '<p>Unable to connect to the ' .
+              'database server at this time.</p>' );
+        }
     // If a score  has been submitted,
     // add it to the database.
-    if (isset($_POST['addscore'])) {
-
-      $sql = 'INSERT INTO ADMIN SET ScoreValue="'.$_POST['addscore'].'", Date=CURDATE()';
-      if ($dbcnx->query($sql)) {
-        echo('<p>Your score has been added.</p>'); 
-      } else {
-        echo('<p>Error adding submitted score: ' .
-             $dbcnx->error_log . '</p>');
-      }
+    if (isset($_GET['addscore'])) { 
+      $sql = 'INSERT INTO ADMIN SET ScoreValue="'.$_GET['addscore'].'", Date=CURDATE()';
+      echo ($sqlObj->query($sql) ? '<p>Your score has been added.</p>'
+                                 : '<p>Error adding submitted score: '.$sqlObj->error_log.'</p>');
     }
 ?>
 
